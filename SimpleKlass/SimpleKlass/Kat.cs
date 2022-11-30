b0a07c2e76b1bd162174197460c2e0d4b5a6a89f
ChimpChonk <75368242+ChimpChonk@users.noreply.github.com>
2022-11-30 08:21:09 +0100

    Simple Class Task

---
+++ SimpleKlass/SimpleKlass/Kat.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleKlass
+{
+    internal class Kat
+    {
+        public string Colour { get; set; }
+
+        public Kat(string _colour)
+        {
+            Colour = _colour;
+        }
+    }
+}
---
+++ SimpleKlass/SimpleKlass/Program.cs
@@ -0,0 +1,10 @@
+﻿using SimpleKlass;
+
+Kat cat = new Kat("Gray");
+
+Console.WriteLine(cat.Colour);
+cat.Colour = "Yellow";
+
+Console.WriteLine(cat.Colour);
+
+Console.ReadKey();
